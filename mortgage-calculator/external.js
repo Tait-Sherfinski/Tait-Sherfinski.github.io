a926355e7a3d9b41423850cf368aeed3a5fd837f
TaitSherfinski Sherfinski <Tsherfin@gmail.com>
2023-04-18 13:40:44 -0500

    update

---
+++ mortgage-calculator/external.js
@@ -0,0 +1,9 @@
+function calc() {
+    var out = document.getElementById("out");
+    out.innerText = "hello"
+}
+
+function reset() {
+    var out = document.getElementById("out");
+    out.innerText = ""
+}
