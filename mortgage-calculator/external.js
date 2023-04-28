c2f0c79826d6b312d78413961413e83ad1b1e2ae
TaitSherfinski Sherfinski <Tsherfin@gmail.com>
2023-04-28 13:16:17 -0500

    update

--- mortgage-calculator/external.js
+++ mortgage-calculator/external.js
@@ -1,11 +1,11 @@
 function calc() {
-    var out = document.getElementById("out");
-    var p = document.getElementById("principal");
-    var r = document.getElementById("intrate");
-    var t = document.getElementById("numyears");
-    var m = document.getElementById("monpay");
+    var out = Number(document.getElementById("out"));
+    var p = Number(document.getElementById("principal"));
+    var r = Number(document.getElementById("intrate"));
+    var t = Number(document.getElementById("numyears"));
+    var m = Number(document.getElementById("monpay"));
     var n = 12;
-    var b = p * (1 + r / n)**n * t - m * [(1 + r / n)**n * t - 1] / (r / n);
+    var b = p * (1 + r / n)**n*t - m * [(1 + r / n)**n*t ]
     out.innerText = "Remaining Loan Balance: " + b;
 }
 
