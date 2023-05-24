5e0d8b54dfb8b821f8946043983c5dbb166070ea
TaitSherfinski Sherfinski <Tsherfin@gmail.com>
2023-05-24 13:43:38 -0500

    update

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function reset() {
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var subject = document.getElementById("subj");
+    var reason = document.getElementById("reas");
+    var message = document.getElementById("mess");
+
+    name.value = "";
+    email.value = "";
+    subject.value = "";
+    reason.value = "";
+    message.value = "";
+}
